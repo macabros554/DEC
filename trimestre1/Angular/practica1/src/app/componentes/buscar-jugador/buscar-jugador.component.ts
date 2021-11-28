311bb237a5f099f5b44f2fe3257e1d0ca77332b5
macabros554 <macabros554@gmail.com>
2021-11-28 19:32:15 +0100

    intento 1 angular

---
+++ trimestre1/Angular/practica1/src/app/componentes/buscar-jugador/buscar-jugador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-jugador',
+  templateUrl: './buscar-jugador.component.html',
+  styleUrls: ['./buscar-jugador.component.css']
+})
+export class BuscarJugadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

84ab302af243327f018f43a58de9299d8bd4c179
macabros554 <macabros554@gmail.com>
2021-11-29 10:07:12 +0100

    practica1

--- trimestre1/Angular/practica1/src/app/componentes/buscar-jugador/buscar-jugador.component.ts
+++ trimestre1/Angular/practica1/src/app/componentes/buscar-jugador/buscar-jugador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Jugador } from '../interfaces/componentes.interface';
+import { ComponentesService } from '../services/componentes.service';
 
 @Component({
   selector: 'app-buscar-jugador',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarJugadorComponent implements OnInit {
 
-  constructor() { }
+  nombre:string =""
+
+  get jugadores():Jugador[]{
+    return this.bsService.jugadores;
+  }
+  jugadoresEncontrados:Jugador[]=[]
+
+  constructor(private bsService:ComponentesService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    this.jugadoresEncontrados=[]
+    for (let i = 0; i < this.jugadores.length; i++) {
+      if (this.jugadores[i].nombre==this.nombre) {
+        this.jugadoresEncontrados.push(this.jugadores[i])
+      }
+
+    }
   }
 
 }
